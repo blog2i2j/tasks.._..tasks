055dddb9327662d859203c66b79a389aa3b484f9
Alex Baker <alex@tasks.org>
2026-02-26 13:02:02 -0600

    Updated chip design
    Use material kolor for tonal colors
    Replace outline and transparent background with solid chip

---
+++ kmp/src/commonMain/kotlin/org/tasks/themes/ChipColors.kt
@@ -0,0 +1,14 @@
+package org.tasks.themes
+
+import com.materialkolor.palettes.TonalPalette
+
+data class ChipColors(val backgroundColor: Int, val contentColor: Int)
+
+fun chipColors(seedColor: Int, isDark: Boolean): ChipColors {
+    val palette = TonalPalette.fromInt(seedColor)
+    return if (isDark) {
+        ChipColors(palette.tone(30), palette.tone(90))
+    } else {
+        ChipColors(palette.tone(90), palette.tone(10))
+    }
+}
